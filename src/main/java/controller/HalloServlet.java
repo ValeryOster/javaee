cd98fd249c25359314da7ac38c54731c5f945ee2
valery <valera_oster@hotmail.com>
2017-04-20 11:40:11 +0200

    Deploy

--- src/main/java/controller/HalloServlet.java
+++ src/main/java/controller/HalloServlet.java
@@ -1,8 +1,9 @@
 package controller;
 
+import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 
-
+@WebServlet
 public class HalloServlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         System.out.println(request.getParameter("name"));
